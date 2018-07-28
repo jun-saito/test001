01aa138bcb3da7d5453ed628ef8483d88cae56f8
junichi Saito <jun-saito@para-flyer.com>
2018-07-29 00:36:08 +0900

    Good Bye Message追加

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace test001
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Good Bye!");
         }
     }
 }
